5800d8a8b4d3553c17028c85afabf752028ea50d
Darián López Utra <daroxs95@gmail.com>
2024-06-06 20:23:31 +0200

    Add placeholder playground page

---
+++ app/routes/playground._index.tsx
@@ -0,0 +1,9 @@
+import styles from "~/styles/playground.module.css";
+
+export default function Portfolio() {
+    return (
+        <main className="main-content">
+            <article className={styles.section}>Playground</article>
+        </main>
+    );
+}
